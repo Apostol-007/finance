4e9aa0f4ba32fadf279f6f956bbe444833381355
Web-Works <127725496+Apostol-007@users.noreply.github.com>
2024-07-12 10:13:42 +0500

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
 
 # Переменные для настройки контейнера
+#DOCKER_IMAGE="apostol-finance"
+#DOCKER_CONTAINER="finance-apostol"
+#GIT_REPO="https://github.com/Apostol-007/finance.git"
+#GIT_BRANCH="master"
+#PROJECT_DIR="finance_project"
+
+# Проверяем, что Docker установлен и доступен
+#docker -v >/dev/null 2>&1
+#if [ $? -ne 0 ]; then
+#  echo "Ошибка: Docker не установлен или не доступен."
+#  exit 1
+#fi
+
+# Удаляем старый контейнер, если он существует
+#docker stop $DOCKER_CONTAINER >/dev/null 2>&1
+#docker rm $DOCKER_CONTAINER >/dev/null 2>&1
+
+# Подгружаем код из репозитория Git
+#git clone -b $GIT_BRANCH $GIT_REPO
+
+#cd finance
+
+# Строим Docker образ из Dockerfile внутри репозитория
+#docker build -t $DOCKER_IMAGE .
+
+# Запускаем Docker контейнер
+#docker run -d -p 8000:8000 --name $DOCKER_CONTAINER $DOCKER_IMAGE
+#echo "Контейнер запущен. Доступен по адресу http://localhost:8000/"
+#!/bin/bash
+
+# Переменные для настройки контейнера
 DOCKER_IMAGE="apostol-finance"
 DOCKER_CONTAINER="finance-apostol"
 GIT_REPO="https://github.com/Apostol-007/finance.git"
 GIT_BRANCH="master"
-#PROJECT_DIR="finance_project"
 
 # Проверяем, что Docker установлен и доступен
 docker -v >/dev/null 2>&1
@@ -14,18 +44,17 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# Удаляем старый контейнер, если он существует
-docker stop $DOCKER_CONTAINER >/dev/null 2>&1
-docker rm $DOCKER_CONTAINER >/dev/null 2>&1
+# Удаляем старые контейнеры, если они существуют
+docker-compose down >/dev/null 2>&1
 
 # Подгружаем код из репозитория Git
 git clone -b $GIT_BRANCH $GIT_REPO
 
 cd finance
 
-# Строим Docker образ из Dockerfile внутри репозитория
-docker build -t $DOCKER_IMAGE .
+chmod +x start.sh
 
-# Запускаем Docker контейнер
-docker run -d -p 8000:8000 --name $DOCKER_CONTAINER $DOCKER_IMAGE
-echo "Контейнер запущен. Доступен по адресу http://localhost:8000/"
+# Запускаем контейнеры с помощью docker-compose
+docker-compose up -d
+
+echo "Контейнеры запущены. Web доступен по адресу http://localhost:8000/"
